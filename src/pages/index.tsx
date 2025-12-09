3301c9e3e037db9deb8b8abc71426aa75105c89d
Serge Basangovs <sergejs.basangovs@artfinder.com>
2025-12-09 09:29:13 +0000

    refactor: init app setup pages router

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+const Homepage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline my-4 text-center">
+        Welcome to the Multi-language Quotes App
+      </h1>
+    </div>
+  )
+}
+
+export default Homepage
